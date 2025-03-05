f61f0daa7659674162b8c986cdbb459636fe93b9
ryota <morozumi.ryota@gmail.com>
2025-03-05 21:11:59 +0900

    管理者ログイン機能

---
+++ src/app/Models/AdminUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class AdminUser extends Authenticatable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $table = 'admin_users';
+}
