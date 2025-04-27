16217605a0223b3ebe37ae536de29992f7171589
ryota <morozumi.ryota@gmail.com>
2025-04-27 12:03:42 +0900

    スタッフ管理一覧の実装

---
+++ src/app/Http/Controllers/AdminListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminListController extends Controller
+{
+    public function show()
+    {
+        $admin = Auth::guard('admin')->user();
+        $staffs = User::all();
+
+        return view('admin.member',compact('staffs'));
+    }
+}
