656520d8a50070a4d829346cdf35077e23ad12b5
ryota <morozumi.ryota@gmail.com>
2025-05-10 23:37:04 +0900

    再修正

--- src/app/Http/Controllers/AdminRequestController.php
+++ src/app/Http/Controllers/AdminRequestController.php
@@ -12,7 +12,6 @@ class AdminRequestController extends Controller
         $user = Auth::guard('admin')->user();
         $unapproved__lists = NewAttendance::withStatus(NewAttendance::STATUS_PENDING)->get();
         $approved__lists = NewAttendance::withStatus(NewAttendance::STATUS_APPROVED)->get();
-
         return view ('admin.request',compact('unapproved__lists','approved__lists'));
     }
 }
